aa1393cb297da5c378badbd21af3477156319f09
tracer-nju <tracer@nju.edu.cn>
2019-11-18 15:27:57 +0800

    Static hostname: ljk-ThinkPad-13          Icon name: computer-laptop            Chassis: laptop         Machine ID: ad3db079daba49adb507255d8c569eaa            Boot ID: 6bf3326eb49f482eaaa77abfe9bfb7f4   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-32-generic       Architecture: x86-64  15:27:57 up 53 min,  1 user,  load average: 0.90, 0.84, 0.80

--- multimod/p1.c
+++ multimod/p1.c
@@ -11,6 +11,7 @@ int64_t add_mod(int64_t a,int64_t b,int64_t m){
 }
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
+  printf("%ld\n",a);
   int64_t a1=a>b? a:b;
   int64_t b1=a<b? a:b;
   a1=a1%m;
