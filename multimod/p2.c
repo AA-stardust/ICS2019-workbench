c9936abe9d09a926aa7ce0533b5e0711351e3c5a
tracer-nju <tracer@nju.edu.cn>
2019-11-20 19:13:11 +0800

    Static hostname: ljk-ThinkPad-13          Icon name: computer-laptop            Chassis: laptop         Machine ID: ad3db079daba49adb507255d8c569eaa            Boot ID: 43bd6df0f2b14c3a94e5294812d59f31   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-32-generic       Architecture: x86-64  19:13:11 up 15 min,  1 user,  load average: 0.29, 0.31, 0.27

--- multimod/p2.c
+++ multimod/p2.c
@@ -52,7 +52,6 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   int64_t b_mod[63];
   int len2=sizeof(b_mod)/sizeof(b_mod[0]);
   init_b(b,m,b_mod,len2);
-  print_b(b_mod,len2);
   assert(len1==len2);
   for(int i=0;i<len1;i++){
     int64_t temp=(int64_t)a_bi[i]*b_mod[i];
