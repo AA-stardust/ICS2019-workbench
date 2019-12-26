85d9eb6c27879d510991bb75d281f25e340078c8
tracer-nju <tracer@nju.edu.cn>
2019-12-26 10:33:25 +0800

    Static hostname: ljk-ThinkPad-13          Icon name: computer-laptop            Chassis: laptop         Machine ID: ad3db079daba49adb507255d8c569eaa            Boot ID: c84a69944d574c71a04e4ae187f04686   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-32-generic       Architecture: x86-64  10:33:25 up 28 min,  1 user,  load average: 0.87, 0.75, 0.70

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -17,7 +17,7 @@ int64_t asm_add(int64_t a, int64_t b) {
   );
   return b;
 }
-int popcnt_test(uint64_t n){
+int popcnt_test(uint64_t x){
   int s = 0;
   for (int i = 0; i < 64; i++) {
     if ((x >> i) & 1) s++;
