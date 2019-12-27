6f3a1c72113375fb44d69ab084de7f3889e5a70e
tracer-nju <tracer@nju.edu.cn>
2019-12-27 14:11:26 +0800

    Static hostname: ljk-ThinkPad-13          Icon name: computer-laptop            Chassis: laptop         Machine ID: ad3db079daba49adb507255d8c569eaa            Boot ID: 6d8b4b531a884bde8dd4c6e2f7ee68ee   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-32-generic       Architecture: x86-64  14:11:26 up  2:13,  1 user,  load average: 1.45, 0.94, 0.61

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -77,7 +77,7 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
     "retq\n\t"   
     :"=g"(dest)
     :
-    :"rdi","rsi","ecx","r8b","rax"
+    :"rdi","rsi","ecx","rax"
   );
   return dest;
 }
