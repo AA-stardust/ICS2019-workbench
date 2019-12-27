65678adda861fa4d45c0d2775af4475a0d53e63a
tracer-nju <tracer@nju.edu.cn>
2019-12-27 15:14:38 +0800

    Static hostname: ljk-ThinkPad-13          Icon name: computer-laptop            Chassis: laptop         Machine ID: ad3db079daba49adb507255d8c569eaa            Boot ID: 6d8b4b531a884bde8dd4c6e2f7ee68ee   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-32-generic       Architecture: x86-64  15:14:39 up  3:16,  1 user,  load average: 0.94, 0.87, 0.76

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -86,15 +86,14 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 
 int asm_setjmp(asm_jmp_buf env) {
   // TODO: implement
-  uint64_t temp=(uint64_t)env;
-  register uint64_t rip0 asm("rbx")=0;
-  register asm_jmp_buf buf asm("rcx")=temp;
-
+  //register uint64_t rip0 asm("rbx")=0;
   asm(
-    "movq 0x8(%%rsp),%0\n\t"
-    :"=r"(rip0)
+    "movq 0x8(%%rsp),%%rbx\n\t"
+    "movq %%rbx,(%0)\n\t"
+    :
+    :"r"(env)
+    :"rbx"
   );
-  printf("%lx\n",rip0);
   return 0;
 }
 
