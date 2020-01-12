5d5590b53a7f27979d5ca1e792e811b7e4e4f225
tracer-nju <tracer@nju.edu.cn>
2020-01-12 22:13:35 +0800

    Static hostname: ljk-ThinkPad-13          Icon name: computer-laptop            Chassis: laptop         Machine ID: ad3db079daba49adb507255d8c569eaa            Boot ID: a174a6801137479e951d8bd3f29073ab   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-38-generic       Architecture: x86-64  22:13:36 up 51 min,  1 user,  load average: 0.25, 0.44, 0.40

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -3,15 +3,52 @@
 
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
-
+static uint64_t hit_number=0;
+static uint64_t cycle=0;
 static uint64_t cycle_cnt = 0;
 
 void cycle_increase(int n) { cycle_cnt += n; }
+typedef struct{
+  bool dirty_bit:1;
+  bool valid_bit:1;
+  uint32_t tag:13;
+  uint8_t data[64];
+}cache_line;
 
+cache_line*Cache;
 // TODO: implement the following functions
 
 uint32_t cache_read(uintptr_t addr) {
-  return 0;
+  uint32_t ret_data=0;
+  uint32_t pa;
+  cycle_increase(1);
+  addr&=0x1ffffff;
+  uint32_t block_offset=addr&0x3c;
+  uint32_t group_number=(addr>>6)&0x3f;
+  uint32_t tag=(addr>>12)&0x1fff;
+  uint32_t blocks_number=(addr>>6)&0x7ffff;
+  uint32_t start=group_number*4;
+  int i=0;
+  int j=0;
+  int flag;
+  for(i=start;i<start+4;i++){
+    if(Cache[i].tag==tag&&Cache[i].valid_bit==1){
+      hit_number++;
+      break;
+    }
+  }
+  if(i<start+4){
+    ret_data=Cache[i].data[block_offset]+(Cache[i].data[block_offset+1]<<8)+(Cache[i].data[block_offset+2]<<16)+(Cache[i].data[block_offset+3]<<24);
+
+  }
+  else{
+    for(j=start;j<start+4;j++){
+      if(Cache[j].valid_bit==0){
+
+      }
+    }
+  }
+  return ret_data;
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
