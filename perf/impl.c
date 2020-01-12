4010c393b552e95d4e0c2e6da75b84854578ce28
tracer-nju <tracer@nju.edu.cn>
2020-01-12 17:40:55 +0800

    Static hostname: ljk-ThinkPad-13          Icon name: computer-laptop            Chassis: laptop         Machine ID: ad3db079daba49adb507255d8c569eaa            Boot ID: f279c8957b904b8b8d93f6d16060d072   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-38-generic       Architecture: x86-64  17:40:55 up  7:24,  1 user,  load average: 0.30, 0.53, 0.48

--- perf/impl.c
+++ perf/impl.c
@@ -128,16 +128,16 @@ int64_t multimod_p3(int64_t a, int64_t b, int64_t m) {
 void p1()
 {
   FILE *fp = fopen("multimod.txt", "r");
-  int64_t a, b, m, ans,count;
+  int64_t a, b, m, ans;
   int sign;
-  count=0;
+  //count=0;
   while(!feof(fp))
   {
     fscanf(fp, "%ld %ld %ld %ld %d", &a, &b, &m, &ans, &sign);
     multimod_p1(a, b ,m);
-    count++;
+    //count++;
   }
-  printf("count: %ld\n",count);
+  //printf("count: %ld\n",count);
 }
 
 void p2()
